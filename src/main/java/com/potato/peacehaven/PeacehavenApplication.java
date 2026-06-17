7bd1f1a740f750a3eee680e00cf9ad4aea1f796a
potato <xhj01830196@cainiao.com>
2026-06-17 13:57:05 +0200

    original commit

---
+++ src/main/java/com/potato/peacehaven/PeacehavenApplication.java
@@ -0,0 +1,13 @@
+package com.potato.peacehaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeacehavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeacehavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potato/peacehaven/PeacehavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potato.peacehaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeacehavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
