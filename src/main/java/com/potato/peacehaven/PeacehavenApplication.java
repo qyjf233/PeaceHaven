45e30b7dc44808563ba0147c46b19048364ef904
potato <2332684858@qq.com>
2026-07-06 02:04:29 +0200

    缓存配置更改

--- src/main/java/com/potato/peacehaven/PeacehavenApplication.java
+++ src/main/java/com/potato/peacehaven/PeacehavenApplication.java
@@ -2,8 +2,10 @@ package com.potato.peacehaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
+@EnableJdbcHttpSession  // 启用 Spring Session JDBC，将 Session 持久化到数据库
 public class PeacehavenApplication {
 
 	public static void main(String[] args) {
